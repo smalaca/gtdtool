a965afa8c20387e2225fcaa3d7bf90ef1dfe4d4d
smalaca <ab13.krakow@gmail.com>
2015-04-30 07:28:17 +0200

    PROJECT - Introduce Jacoco for code coverage

---
+++ src/test/java/com/smalaca/gtdtool/rest/api/ProjectControllerTest.java
@@ -0,0 +1,20 @@
+package com.smalaca.gtdtool.rest.api;
+
+import com.smalaca.gtdtool.domain.Project;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ProjectControllerTest {
+    private final ProjectController controller = new ProjectController();
+    
+    @Test
+    public void shouldCreateProjectWithGivenName() {
+        String projectName = "Some fancy name";
+        Project project = controller.project(projectName);
+
+        assertThat(project.getId(), is(1L));
+        assertThat(project.getName(), is(projectName));
+    }
+}
